f0a59a1ab05b89bbfec18b8ca1a2df2a1ec44646
Mauricio Montiel <mauriciomontiel34@gmail.com>
2018-03-02 19:12:01 -0300

    Adição de novas páginas

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ var app = angular.module('myApp', ['ui.router']);
 app.controller('customersCtrl', function($scope, $http) {
   $http.get("menuData.html").then(function (response) {
       $scope.combos = response.data.combos;
-      $scope.pizzas = response.data.pizzas;
+      $scope.pizzas = response.data.pizzas[0].tamanhos;
+      $scope.saboresSalgados = response.data.pizzas[2].saboresSalgados;
       $scope.bebidas = response.data.bebidas;
   });
+
+  $scope.bordas = [{"borda":"Catupiry", "preco": "R$ 5"}, {"borda":"Cheddar", "preco": "R$ 5"},{"borda":"Sem Borda", "preco": ""}];
+  $scope.massas = [{"massa":"Fina", "preco": ""}, {"massa":"Normal", "preco": ""},{"massa":"Grossa", "preco": "R$ 2"}];
+
 });
