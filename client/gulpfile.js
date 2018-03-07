c7297e52b23cdfd35919e896ae1be1248ba61c71
Marlon de Oliveira dos Santos <marlonlolz75@gmail.com>
2018-03-07 16:19:21 -0300

    troca de permissões (ubuntu)

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -39,7 +39,8 @@ var path = {
     'node_modules/angular-tooltips/dist/angular-tooltips.min.css',
     'node_modules/ng-dialog/css/ngDialog-theme-default.min.css',
     'node_modules/ng-table/bundles/ng-table.min.css',
-    'node_modules/sweetalert2/dist/sweetalert2.min.css'
+    'node_modules/sweetalert2/dist/sweetalert2.min.css',
+    'node_modules/angularjs-toaster/toaster.min.css'
   ],
   IMG: [
   	'src/img/**'
@@ -51,7 +52,8 @@ var path = {
     'node_modules/angular-animate/angular-animate.min.js',
     'node_modules/ng-dialog/js/ngDialog.js',
     'node_modules/angular-tooltips/dist/angular-tooltips.min.js',
-    'node_modules/sweetalert2/dist/sweetalert2.min.js'
+    'node_modules/sweetalert2/dist/sweetalert2.min.js',
+    'node_modules/angularjs-toaster/toaster.min.js'
   ],
   DIST: './dist'
 };
